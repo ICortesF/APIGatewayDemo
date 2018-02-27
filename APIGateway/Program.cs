df6e3b5f1ace9edb2f0b50025d474443f7219326
Israel <israel@borsan.es>
2018-02-27 18:35:31 +0100

    Add project files.

---
+++ APIGateway/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System;
+
+namespace APIGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new WebHostBuilder()
+               .UseKestrel()
+               .UseContentRoot(Directory.GetCurrentDirectory())
+               .ConfigureAppConfiguration((hostingContext, config) =>
+               {
+                   config
+                       .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                       .AddJsonFile("appsettings.json", true, true)
+                       .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                       .AddJsonFile("configuration.json")
+                       .AddEnvironmentVariables();
+
+               })
+               .ConfigureServices(s => {
+                   s.AddOcelot();
+
+               })
+               .ConfigureLogging((hostingContext, logging) =>
+               {
+                   //add your logging
+               })
+               .UseIISIntegration()
+               .Configure(app =>
+               {
+                   app.UseOcelot().Wait();
+
+               }).UseUrls("http://localhost:9000")
+               .Build()
+               .Run();
+        }
+    }
+}
